a4b7ec780ac9a7a5a56a0760ae58cb272d5d5502
milbub <mil.bubniak@gmail.com>
2024-05-09 17:04:47 +0200

    Changed PWD input data format once again

--- meteoflux.py
+++ meteoflux.py
@@ -102,11 +102,11 @@ def write_to_influxdb(influx_write_api, data) -> None:
                 case "NABAT_I":
                     name = "volt_min"
                 case "PWD_V01":
-                    if float(value) < 0:
+                    if float(value) < 0 or float(value) > 20000:
                         continue
                     name = "pwd_visibility"
                 case "PWD_V10":
-                    if float(value) < 0:
+                    if float(value) < 0 or float(value) > 20000:
                         continue
                     name = "pwd_visibility_10m"
                 case "PWD_P01":
@@ -121,20 +121,18 @@ def write_to_influxdb(influx_write_api, data) -> None:
                     if float(value) < 0:
                         continue
                     name = "pwd_water_intensity"
-                    value = value / 100
                 case "PWD_WS":
                     if float(value) < 0:
                         continue
                     name = "pwd_water_sum"
-                    value = value / 100
                 case "PWD_SS":
                     if float(value) < 0:
                         continue
                     name = "pwd_snow_sum"
                 case "PWD_T":
                     name = "pwd_temp"
-                    value = value / 100
-                    if (value > 100) or (-9.9 > value > -10):
+                    value = float(value)
+                    if (value > 100) or (value < -100):
                         continue
                 case "PWD_ERR":
                     name = "pwd_error"
